0e39e303f02bbda328a86219eb691dde8370f610
Robert G. Raleigh <robert@helloworlddevs.com>
2018-01-23 22:39:57 -0800

    updated brunch config

--- brunch-config.js
+++ brunch-config.js
@@ -1,5 +1,49 @@
 // See http://brunch.io for documentation.
 exports.files = {
-  javascripts: {joinTo: 'app.js'},
-  stylesheets: {joinTo: 'app.css'}
+  javascripts: {
+    joinTo: {
+    'js/vendor.js': /(?!app)/,
+    'js/app.js': /^app/
+    }
+  },
+  stylesheets: {
+    joinTo: 'app.css'
+  }
+};
+
+exports.plugins = {
+  babel: {
+    presets: ['env']
+  },
+  sass: {
+    options: {
+      includePaths: [
+        'node_modules/semantic-ui-sass/scss',
+        'app/scss/custom'
+      ],
+      precision: 8
+    }
+  },
+  copycat: {
+    verbose: true,
+    onlyChanged: true,
+    "fonts": [
+
+    ],
+    "images": [
+      "app/assets/img"
+    ],
+    "media": [
+      "app/assets/media"
+    ]
+  }
+};
+
+exports.npm = {
+  enabled: true,
+  compilers: ["babel-brunch"],
+  globals: {
+    $: 'jQuery',
+    jQuery: 'jQuery'
+  }
 };
