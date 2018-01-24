7e8a6be837b0d8ae2ef23a693f90f4ac1abf5b6c
Robert G. Raleigh <robert@helloworlddevs.com>
2018-01-23 22:08:51 -0800

    updated scss skeleton files

--- app/initialize.js
+++ app/initialize.js
@@ -1,4 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-  // do your setup here
-  console.log('Initialized app');
-});
+// initialize.js
+/* jshint esversion: 6 */
+
+/*
+  import brunch package npm dependencies below
+  use either the import or require syntax
+  import 'package folder name in node_modules';
+  require(package folder name in node_modules'');
+*/
+import 'jquery';
+import 'semantic-ui-sass';
+
+//
+$(function () {
+  document.addEventListener('DOMContentLoaded', function() {
+    // do your setup here
+    console.log('Initialized brunch app');
+  });
+}());
